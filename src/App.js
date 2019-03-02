940d38522abf5c67ae23a38bc7871a6d609bdb2e
Ado Moshe <adoran.moshe@gmail.com>
2019-03-02 14:12:23 -0800

    Imported LogIn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import LogIn from './pages/LogIn';
+
 class App extends Component {
   render() {
     return (
       <div>
-      
+      <LogIn />
       </div>
     );
   }
---
+++ src/pages/LogIn.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LogIn extends Component {
+    componentDidMount() {
+      window.scrollTo(0, 0);
+    }
+    render() {
+      return (
+        <div>
+<button>HI</button>
+        </div>
+      );
+    }
+  }
+
+  export default LogIn;
