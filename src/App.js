b2162d9e0d1bf4574368aa0eb5957e2d6ccfb784
Ado Moshe <adoran.moshe@gmail.com>
2019-03-02 15:07:15 -0800

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import LogIn from './pages/LogIn';
+import Create from './pages/create/Create';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <LogIn />
+      <Router>
+      <div className="container-fluid">
+      <Switch>
+        <Route exact path="/" component={LogIn} />
+        <Route exact path="/create" component={Create} />
+      </Switch>
       </div>
+  </Router>
     );
   }
 }
