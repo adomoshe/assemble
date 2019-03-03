ff52479f8eaca3f70d66e84d4caa6834bae7bcc9
Ado Moshe <adoran.moshe@gmail.com>
2019-03-02 18:09:54 -0800

    Added logos

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -7,25 +7,25 @@ import logo3 from '../logo3.svg';
 const styles = { logo: { height: '10vh', width: '60vh' } };
 
 class LogIn extends Component {
-    componentDidMount() {
-      window.scrollTo(0, 0);
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-
-    
-    }
-    render() {
-      return (
+  render() {
+    return (
+      <div>
+        <img src={logo1} alt="assemble SVG" style={styles.logo} />
+        <img src={logo2} alt="assemble SVG" style={styles.logo} />
+        <img src={logo3} alt="assemble SVG" style={styles.logo} />
         <div id="container">
-          <div id="login-button"></div>
+          <div id="login-button" />
           <div id="profile" className="hidden">
-            <img id="picture" src="#"/>
             <p id="welcome">Welcome!</p>
             <h3> hello</h3>
           </div>
         </div>
-      );
-    }
-
+      </div>
+    );
   }
 }
 
