e2a4753b2acb9dbd90fe5e0abcbc5e6a2e4fd700
Ado Moshe <adoran.moshe@gmail.com>
2019-03-03 04:41:08 -0800

    Mapping

--- client/src/pages/confirmation/Confirmation.js
+++ client/src/pages/confirmation/Confirmation.js
@@ -6,42 +6,42 @@ import API from '../../utils/API';
 const styles = { logo: { height: '10vh', width: '60vh' } };
 
 class Confirmation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { events: '' };
+  }
+
   componentWillMount() {
-    API.getEvents().then(res => this.setState({ events: res }));
+    API.getEvents().then(res => this.setState({ events: res.data }));
   }
   componentDidMount() {
     window.scrollTo(0, 0);
-    document.getElementById('container').style.display = 'none';
+    // document.getElementById('container').style.display = 'none';
   }
 
   render() {
     const events = this.state.events;
 
     return (
-      <div id="container">
+      <div id="">
         <h1>
           Your <img src={logo3} alt="assemble SVG" style={styles.logo} /> has
           been scheduled!
         </h1>
         <br />
         <br />
-        {events.map((event, index) => {
-          return (
-            <table id="eventsTable">
-              <thead>
-                <tr>
-                  <td>Title: {event.title}</td>
-                  <td>Date: {event.date}</td>
-                  <td>Location: {event.location}</td>
-                  {event.description && (
-                    <td>Description: {event.description}</td>
-                  )}
-                </tr>
-              </thead>
-              <tbody id="plannedAsemblies" />
-            </table>
-          );
-        })}
+
+        {events &&
+          events.map((event, index) => {
+            return (
+              <ul key={index}>
+                <li>Title: {event.title}</li>
+                <li>Date: {event.date}</li>
+                <li>Location: {event.location}</li>
+                {event.description && <li>Description: {event.description}</li>}
+              </ul>
+            );
+          })}
       </div>
     );
   }
