4c2006800b45e4885801894bed3e2edb9510da96
Ado Moshe <adoran.moshe@gmail.com>
2019-03-02 16:28:56 -0800

    Small changes

---
+++ src/pages/create/Create.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Create extends Component {
+    componentDidMount() {
+      window.scrollTo(0, 0);
+    }
+    render() {
+      return (
+        <div>
+        <h1>Welcome (Name)</h1>
+        <h2>What are you assembling?</h2>
+        
+        </div>
+      );
+    }
+  }
+
+  export default Create;
