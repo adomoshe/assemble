d262400bc738ea73cc576c0cbb2441c0cf76e342
Ado Moshe <adoran.moshe@gmail.com>
2019-03-02 18:33:43 -0800

    Confirmation page

---
+++ src/pages/confirmation/Confirmation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const styles = { logo: { height: '10vh', width: '60vh' } };
+
+class Confirmation extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Confirmation;
