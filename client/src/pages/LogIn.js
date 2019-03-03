d8a24f86336a5be21545d4326002449c8d00c752
Ado Moshe <adoran.moshe@gmail.com>
2019-03-03 07:03:09 -0800

    Add

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,55 +1,40 @@
 import React, { Component } from 'react';
 
-// import logo1 from '../logo1.svg';
-// import logo2 from '../logo2.svg';
 import logo3 from '../logo3.svg';
 
-const styles = { logo: { height: '10vh', width: '60vh' } };
+const styles = {
+  root: { marginTop: '10vh' },
+  logo: { height: '10vh', width: '60vh' }
+};
 
 class LogIn extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
-    var elem = document.getElementById('picture');
-    elem.style.top = '50px';
-    elem.style.left = '50px';
   }
 
   render() {
     return (
-      <div>
-        <div>
-          <div id="container">
-            <h1 className="AsembleFont">
-              <strong>Welcome to </strong>
-              <img src={logo3} alt="assemble SVG" style={styles.logo} />
-            </h1>
-            <br />
-            <br />
-            <br />
-            <center>
-              <ul className="AsembleFont">
-                <li className="frame" id="moveRight">
-                  <strong>Plan hangouts with your friends</strong>
-                </li>
-                <br />
-                <br />
-                <li className="frame" id="moveLeft">
-                  <strong>Organize events for your surroundings</strong>
-                </li>
-                <br />
-                <br />
-                <li className="frame" id="moveRight">
-                  <strong>Assemble with your Community</strong>
-                </li>
-              </ul>
-            </center>
-            <br />
-            <br />
-            <br />
-
-            <div id="login-button" />
-            <div id="profile" className="hidden" />
-          </div>
+      <div className="row justify-content-center" style={styles.root}>
+        <div className="col-sm-6">
+          <h1 className="AsembleFont">
+            <strong>Welcome to </strong>
+            <img src={logo3} alt="assemble SVG" style={styles.logo} />
+          </h1>
+          <center>
+            <div className="AsembleFont">
+              <h4 className="frame" id="moveRight">
+                <strong>Plan hangouts with your friends</strong>
+              </h4>
+              <h4 className="frame" id="moveLeft">
+                <strong>Organize events in your area</strong>
+              </h4>
+              <h4 className="frame" id="moveRight">
+                <strong>Assemble with your Community!</strong>
+              </h4>
+            </div>
+          </center>
+          <div id="login-button" />
+          <div id="profile" className="hidden" />
         </div>
       </div>
     );
