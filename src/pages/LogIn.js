e1b31d3932f6b782e30fbb4794d9667c5e552a19
Shahaf Dan <dan.shachaf@gmail.com>
2019-03-02 17:58:21 -0800

    test2

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react';
 class LogIn extends Component {
     componentDidMount() {
       window.scrollTo(0, 0);
+
+
+    
     }
     render() {
       return (
         <div id="container">
           <div id="login-button"></div>
           <div id="profile" className="hidden">
-            <img id="picture" src="#"></img> 
+            <img id="picture" src="#"/>
             <p id="welcome">Welcome!</p>
             <h3> hello</h3>
           </div>
